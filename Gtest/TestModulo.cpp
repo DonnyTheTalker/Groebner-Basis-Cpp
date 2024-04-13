2b853f213680fde3f4324a087d645dcb4f2d3df8
DonnyTheTalker <maks.egorov.55@mail.ru>
2024-04-13 11:01:59 +0300

    Change Modulo

--- Gtest/TestModulo.cpp
+++ Gtest/TestModulo.cpp
@@ -1,169 +1,170 @@
-#include "gtest/gtest.h"
 #include "Modulo.h"
+#include "gtest/gtest.h"
 
+namespace Groebner::GroebnerTest {
 TEST(ModuloBasic, Construction) {
-    EXPECT_NO_THROW(Modulo(2, 0));
-    EXPECT_NO_THROW(Modulo(2));
-    EXPECT_NO_THROW(Modulo(5, 2));
-    EXPECT_NO_THROW(Modulo(7, 10));
-    EXPECT_NO_THROW(Modulo(100049, 20));
-    EXPECT_NO_THROW(Modulo(3, -1));
+    EXPECT_NO_THROW(Modulo<2>(0));
+    EXPECT_NO_THROW(Modulo<2>());
+    EXPECT_NO_THROW(Modulo<5>(2));
+    EXPECT_NO_THROW(Modulo<7>(10));
+    EXPECT_NO_THROW(Modulo<100049>(20));
+    EXPECT_NO_THROW(Modulo<3>(-1));
 
-    EXPECT_DEATH(Modulo(0), "Order must be positive");
-    EXPECT_DEATH(Modulo(-200), "Order must be positive");
-    EXPECT_DEATH(Modulo(1), "Order must be prime number");
-    EXPECT_DEATH(Modulo(4), "Order must be prime number");
-    EXPECT_DEATH(Modulo(8), "Order must be prime number");
-    EXPECT_DEATH(Modulo(100000), "Order must be prime number");
+    EXPECT_DEATH(Modulo<0>(), "Modulus must be prime number");
+    EXPECT_DEATH(Modulo<1>(), "Modulus must be prime number");
+    EXPECT_DEATH(Modulo<4>(), "Modulus must be prime number");
+    EXPECT_DEATH(Modulo<8>(), "Modulus must be prime number");
+    EXPECT_DEATH(Modulo<100000>(), "Modulus must be prime number");
 }
 
 TEST(ModuloBasic, Normalization) {
-    EXPECT_EQ(Modulo(7, 0), Modulo(7, 7));
-    EXPECT_EQ(Modulo(7, -1), Modulo(7, 6));
-    EXPECT_EQ(Modulo(7, 17), Modulo(7, 3));
-    EXPECT_EQ(Modulo(7, 17), Modulo(7, -4));
-    EXPECT_EQ(Modulo(7, 17), Modulo(7, -11));
+    Modulo<7>(-1);
+    EXPECT_EQ(Modulo<7>(0), Modulo<7>(7));
+    EXPECT_EQ(Modulo<7>(-1), Modulo<7>(6));
+    EXPECT_EQ(Modulo<7>(3), Modulo<7>(17));
+    EXPECT_EQ(Modulo<7>(-4), Modulo<7>(17));
+    EXPECT_EQ(Modulo<7>(-11), Modulo<7>(17));
 }
 
 TEST(ModuloBasic, Getters) {
-    Modulo x(7, -5);
-    EXPECT_EQ(x.Value(), 2);
+    Modulo<7> x(-5);
+    EXPECT_EQ(x.GetValue(), 2);
     EXPECT_FALSE(x.IsZero());
 
-    x = Modulo(7, 200);
-    EXPECT_EQ(x.Value(), 4);
+    x = Modulo<7>(200);
+    EXPECT_EQ(x.GetValue(), 4);
     EXPECT_FALSE(x.IsZero());
 
-    x = Modulo(7, 0);
-    EXPECT_EQ(x.Value(), 0);
+    x = Modulo<7>(0);
+    EXPECT_EQ(x.GetValue(), 0);
     EXPECT_TRUE(x.IsZero());
 
-    x = Modulo(7, 14);
-    EXPECT_EQ(x.Value(), 0);
+    x = Modulo<7>(14);
+    EXPECT_EQ(x.GetValue(), 0);
     EXPECT_TRUE(x.IsZero());
 }
 
 TEST(ModuloBasic, Comparison) {
-    EXPECT_TRUE(Modulo(13, 1) < Modulo(13, 2));
-    EXPECT_TRUE(Modulo(13, 1) < Modulo(13, 28));
-    EXPECT_TRUE(Modulo(13, 1) <= Modulo(13, 1));
-    EXPECT_TRUE(Modulo(13, 1) <= Modulo(13, 2));
-    EXPECT_TRUE(Modulo(13, 1) == Modulo(13, 1));
-    EXPECT_TRUE(Modulo(13, 1) != Modulo(13, 2));
-    EXPECT_TRUE(Modulo(13, 1) != Modulo(7, 1));
-    EXPECT_TRUE(Modulo(13, 1) > Modulo(13, 0));
-    EXPECT_TRUE(Modulo(13, 1) >= Modulo(13, 0));
-    EXPECT_TRUE(Modulo(13, 1) >= Modulo(13, 1));
+    EXPECT_TRUE(Modulo<13>(1) < Modulo<13>(2));
+    EXPECT_TRUE(Modulo<13>(1) < Modulo<13>(28));
+    EXPECT_TRUE(Modulo<13>(1) <= Modulo<13>(1));
+    EXPECT_TRUE(Modulo<13>(1) <= Modulo<13>(2));
+    EXPECT_TRUE(Modulo<13>(1) == Modulo<13>(1));
+    EXPECT_TRUE(Modulo<13>(1) != Modulo<13>(2));
+    EXPECT_TRUE(Modulo<13>(1) > Modulo<13>(0));
+    EXPECT_TRUE(Modulo<13>(1) >= Modulo<13>(0));
+    EXPECT_TRUE(Modulo<13>(1) >= Modulo<13>(1));
 }
 
 TEST(ModuloArithmetics, Inversion) {
     {
-        Modulo x(7, 1);
+        Modulo<7> x(1);
 
-        Modulo y(7, 0);
+        Modulo<7> y(0);
         EXPECT_DEATH(x / y, "Can't divide by zero");
 
-        y = Modulo(7, 1);
-        EXPECT_EQ((x / y).Value(), 1);
+        y = Modulo<7>(1);
+        EXPECT_EQ((x / y).GetValue(), 1);
 
-        y = Modulo(7, 2);
-        EXPECT_EQ((x / y).Value(), 4);
+        y = Modulo<7>(2);
+        EXPECT_EQ((x / y).GetValue(), 4);
 
-        y = Modulo(7, 3);
-        EXPECT_EQ((x / y).Value(), 5);
+        y = Modulo<7>(3);
+        EXPECT_EQ((x / y).GetValue(), 5);
 
-        y = Modulo(7, 4);
-        EXPECT_EQ((x / y).Value(), 2);
+        y = Modulo<7>(4);
+        EXPECT_EQ((x / y).GetValue(), 2);
 
-        y = Modulo(7, 5);
-        EXPECT_EQ((x / y).Value(), 3);
+        y = Modulo<7>(5);
+        EXPECT_EQ((x / y).GetValue(), 3);
 
-        y = Modulo(7, 6);
-        EXPECT_EQ((x / y).Value(), 6);
+        y = Modulo<7>(6);
+        EXPECT_EQ((x / y).GetValue(), 6);
     }
 
     {
-        Modulo x(13, 1);
-        Modulo y(13, 2);
-        EXPECT_EQ((x / y).Value(), 7);
+        Modulo<13> x(1);
+        Modulo<13> y(2);
+        EXPECT_EQ((x / y).GetValue(), 7);
 
-        y = Modulo(13, 3);
-        EXPECT_EQ((x / y).Value(), 9);
+        y = Modulo<13>(3);
+        EXPECT_EQ((x / y).GetValue(), 9);
 
-        y = Modulo(13, 7);
-        EXPECT_EQ((x / y).Value(), 2);
+        y = Modulo<13>(7);
+        EXPECT_EQ((x / y).GetValue(), 2);
     }
 
     {
-        Modulo x(100049, 1);
-        Modulo y(100049, 333);
-        EXPECT_EQ((x / y).Value(), 88632);
+        Modulo<100049> x(1);
+        Modulo<100049> y(333);
+        EXPECT_EQ((x / y).GetValue(), 88632);
 
-        y = Modulo(100049, 12312);
-        EXPECT_EQ((x / y).Value(), 86649);
+        y = Modulo<100049>(12312);
+        EXPECT_EQ((x / y).GetValue(), 86649);
     }
 }
 
 TEST(ModuloArithmetics, Addition) {
-    Modulo x(13, 0);
-    EXPECT_EQ(x += Modulo(13, 21), Modulo(13, 8));
-    EXPECT_EQ(x += Modulo(13, -3), Modulo(13, 5));
-    EXPECT_EQ(x += Modulo(13, 5), Modulo(13, 10));
-    EXPECT_EQ(x += x, Modulo(13, 7));
+    Modulo<13> x;
+    EXPECT_EQ(x += Modulo<13>(21), Modulo<13>(8));
+    EXPECT_EQ(x += Modulo<13>(-3), Modulo<13>(5));
+    EXPECT_EQ(x += Modulo<13>(5), Modulo<13>(10));
+    EXPECT_EQ(x += x, Modulo<13>(7));
 }
 
 TEST(ModuloArithmetics, Substitution) {
-    Modulo x(13, 0);
-    EXPECT_EQ(x -= Modulo(13, 21), Modulo(13, 5));
-    EXPECT_EQ(x -= Modulo(13, -3), Modulo(13, 8));
-    EXPECT_EQ(x -= Modulo(13, 9), Modulo(13, 12));
-    EXPECT_EQ(x -= x, Modulo(13, 0));
+    Modulo<13> x;
+    EXPECT_EQ(x -= Modulo<13>(21), Modulo<13>(5));
+    EXPECT_EQ(x -= Modulo<13>(-3), Modulo<13>(8));
+    EXPECT_EQ(x -= Modulo<13>(9), Modulo<13>(12));
+    EXPECT_EQ(x -= x, Modulo<13>(0));
 }
 
 TEST(ModuloArithmetics, Multiplication) {
-    Modulo x(13, 1);
-    EXPECT_EQ(x *= Modulo(13, 21), Modulo(13, 8));
-    EXPECT_EQ(x *= Modulo(13, -3), Modulo(13, 2));
-    EXPECT_EQ(x *= Modulo(13, 55), Modulo(13, 6));
-    EXPECT_EQ(x *= x, Modulo(13, 10));
+    Modulo<13> x(1);
+    EXPECT_EQ(x *= Modulo<13>(21), Modulo<13>(8));
+    EXPECT_EQ(x *= Modulo<13>(-3), Modulo<13>(2));
+    EXPECT_EQ(x *= Modulo<13>(55), Modulo<13>(6));
+    EXPECT_EQ(x *= x, Modulo<13>(10));
 }
 
 TEST(ModuloArithmetics, Division) {
-    Modulo x(13, 1);
-    EXPECT_EQ(x /= Modulo(13, 5), Modulo(13, 8));
-    EXPECT_EQ(x /= Modulo(13, 12), Modulo(13, 5));
-    EXPECT_EQ(x /= Modulo(13, -3), Modulo(13, 7));
-    EXPECT_EQ(x /= x, Modulo(13, 1));
+    Modulo<13> x(1);
+    EXPECT_EQ(x /= Modulo<13>(5), Modulo<13>(8));
+    EXPECT_EQ(x /= Modulo<13>(12), Modulo<13>(5));
+    EXPECT_EQ(x /= Modulo<13>(-3), Modulo<13>(7));
+    EXPECT_EQ(x /= x, Modulo<13>(1));
 }
 
 TEST(ModuloArithmetics, General) {
-    Modulo x(13, 1);
-    Modulo y(13, 5);
-    Modulo z(13, 3);
+    Modulo<13> x(1);
+    Modulo<13> y(5);
+    Modulo<13> z(3);
 
-    EXPECT_EQ(x + y - z, Modulo(13, 3));
-    EXPECT_EQ((x + y) * z, Modulo(13, 5));
-    EXPECT_EQ(x * z + y * z, Modulo(13, 5));
-    EXPECT_EQ((x + y) / z, Modulo(13, 2));
-    EXPECT_EQ(x / z + y / z, Modulo(13, 2));
+    EXPECT_EQ(x + y - z, Modulo<13>(3));
+    EXPECT_EQ((x + y) * z, Modulo<13>(5));
+    EXPECT_EQ(x * z + y * z, Modulo<13>(5));
+    EXPECT_EQ((x + y) / z, Modulo<13>(2));
+    EXPECT_EQ(x / z + y / z, Modulo<13>(2));
     EXPECT_EQ(x + x, x * z - x);
 }
 
 TEST(ModuloArithmetics, UnaryMinus) {
     {
-        Modulo x(7, 1);
-        EXPECT_EQ(-x, Modulo(7, 6));
+        Modulo<7> x(1);
+        EXPECT_EQ(-x, Modulo<7>(6));
     }
 
     {
-        Modulo x(7, -1);
-        EXPECT_EQ(-x, Modulo(7, 1));
+        Modulo<7> x(-1);
+        EXPECT_EQ(-x, Modulo<7>(1));
     }
 
     {
-        Modulo x(7, 3);
-        EXPECT_EQ(-x, Modulo(7, -3));
+        Modulo<7> x(3);
+        EXPECT_EQ(-x, Modulo<7>(-3));
         EXPECT_EQ(-(-x), x);
-        EXPECT_EQ(-(-x), Modulo(7, 3));
+        EXPECT_EQ(-(-x), Modulo<7>(3));
     }
 }
+}  // namespace Groebner::GroebnerTest
--- src/Modulo.h
+++ src/Modulo.h
@@ -1,48 +1,170 @@
 #pragma once
 
+#include <cassert>
 #include <cinttypes>
+#include <cstdlib>
 
+namespace Groebner {
+namespace GroebnerDetails {
+    constexpr bool IsPrime(int64_t n) {
+        if (n <= 1 || (n > 2 && n % 2 == 0)) {
+            return false;
+        }
+
+        for (size_t i = 3; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    using ModuloValueType = int64_t;
+
+    ModuloValueType FindGcdExtended(ModuloValueType x, ModuloValueType y,
+                                    ModuloValueType* coef_x,
+                                    ModuloValueType* coef_y) {
+        assert(coef_x && coef_y);
+
+        if (x == 0) {
+            *coef_x = 0;
+            *coef_y = 1;
+            return y;
+        }
+
+        ModuloValueType temp_x, temp_y;
+        ModuloValueType gcd = FindGcdExtended(y % x, x, &temp_x, &temp_y);
+        *coef_x = temp_y - (y / x) * temp_x;
+        *coef_y = temp_x;
+        return gcd;
+    }
+}  // namespace GroebnerDetails
+
+template <int64_t Modulus>
 class Modulo {
-public:
-    using OrderType = int64_t;
-    using ValueType = int64_t;
-public:
-    Modulo() = delete;
-    explicit Modulo(OrderType order, ValueType value = 0);
-    Modulo(const Modulo &other) = default;
-    Modulo(Modulo &&other) noexcept;
-    Modulo &operator=(const Modulo &other);
-    Modulo &operator=(Modulo &&other) noexcept;
+    public:
+        using ValueType = GroebnerDetails::ModuloValueType;
 
-public:
-    ValueType Value() const;
-    Modulo operator-() const;
-    bool IsZero() const;
+        explicit Modulo(ValueType value = 0) : value_(value) {
+            assert(GroebnerDetails::IsPrime(Modulus) &&
+                   "Modulus must be prime number");
+            Normalize();
+        }
 
-    Modulo &operator+=(const Modulo &other);
-    Modulo &operator-=(const Modulo &other);
-    Modulo &operator*=(const Modulo &other);
-    Modulo &operator/=(const Modulo &other);
+        ValueType GetValue() const { return value_; }
 
-    Modulo operator+(const Modulo &other) const;
-    Modulo operator-(const Modulo &other) const;
-    Modulo operator*(const Modulo &other) const;
-    Modulo operator/(const Modulo &other) const;
+        bool IsZero() const { return value_ == 0; }
 
-    bool operator==(const Modulo &other) const;
-    bool operator<(const Modulo &other) const;
-    bool operator<=(const Modulo &other) const;
-    bool operator>(const Modulo &other) const;
-    bool operator>=(const Modulo &other) const;
+        //        size_t GetModulus() const {
+        //            return Modulus;
+        //        }
 
-private:
-    void Normalize();
-    Modulo Inverse() const;
+        Modulo operator-() const { return Modulo<Modulus>(-value_); }
+        Modulo operator+() const { return *this; }
 
-private:
-    // considering order_ to be positive and prime
-    // 0 <= value_ < order_
-    const OrderType order_;
-    ValueType value_;
+        Modulo& operator+=(const Modulo& other) {
+            value_ += other.value_;
+            Normalize();
+            return *this;
+        }
+
+        Modulo& operator-=(const Modulo& other) {
+            value_ -= other.value_;
+            Normalize();
+            return *this;
+        }
+
+        Modulo& operator*=(const Modulo& other) {
+            value_ *= other.value_;
+            Normalize();
+            return *this;
+        }
+
+        Modulo& operator/=(const Modulo& other) {
+            assert(other.value_ != 0 && "Can't divide by zero");
+            value_ *= other.GetInversed().value_;
+            Normalize();
+            return *this;
+        }
+
+        Modulo operator+(const Modulo& other) const {
+            Modulo temp(*this);
+            temp += other;
+            return temp;
+        }
+
+        Modulo operator-(const Modulo& other) const {
+            Modulo temp(*this);
+            temp -= other;
+            return temp;
+        }
+
+        Modulo operator*(const Modulo& other) const {
+            Modulo temp(*this);
+            temp *= other;
+            return temp;
+        }
+
+        Modulo operator/(const Modulo& other) const {
+            Modulo temp(*this);
+            temp /= other;
+            return temp;
+        }
+
+        bool operator==(const Modulo& other) const {
+            return value_ == other.value_;
+        }
+
+        bool operator!=(const Modulo& other) const {
+            return !(*this == other);
+        }
+
+        bool operator<(const Modulo& other) const {
+            return value_ < other.value_;
+        }
+
+        bool operator<=(const Modulo& other) const {
+            return (*this < other) || (*this == other);
+        }
+
+        bool operator>(const Modulo& other) const {
+            return !(*this <= other);
+        }
+
+        bool operator>=(const Modulo& other) const {
+            return (*this > other) || (*this == other);
+        }
+
+    private:
+        void Normalize() {
+            if (value_ < 0) {
+                value_ = (value_ % Modulus + Modulus) % Modulus;
+            } else {
+                value_ = value_ % Modulus;
+            }
+        }
+
+        Modulo GetNormalized() { return Modulo(*this); }
+
+        void Inverse() {
+            assert(value_ != 0 && "No inverse element for zero");
+            ValueType x, y;
+            GroebnerDetails::FindGcdExtended(value_, Modulus, &x, &y);
+            value_ = x;
+            Normalize();
+        }
+
+        Modulo GetInversed() const {
+            Modulo temp(*this);
+            temp.Inverse();
+            return temp;
+        }
+
+    private:
+        // considering modulus_ to be positive and prime
+        // 0 <= value_ < Modulus
+        ValueType value_;
 };
 
+}  // namespace Groebner
